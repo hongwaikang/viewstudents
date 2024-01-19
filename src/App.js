ed3e5c472de95c2b9eed3e32f05e6a69333ff322
hongwaikang <78042321+hongwaikang@users.noreply.github.com>
2024-01-19 18:34:31 +0800

    changed .env to a config.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { createPool } from '@vercel/postgres';
 
-function App() {
+const config = require('./config.js');
+
+const pool = createPool({
+  connectionString: config.POSTGRES_URL
+  //connectionString: "postgres://default:DAE7NCBvhP9V@ep-rough-unit-92773982-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb",
+});
+
+const App = () => {
+  const [students, setStudents] = useState([]);
+  console.log(process.env.POSTGRES_URL);
+
+  useEffect(() => {
+    const fetchStudents = async () => {
+      try {
+        const { rows } = await pool.sql`SELECT id, firstname, lastname FROM students;`;
+        setStudents(rows);
+      } catch (error) {
+        console.error('Error fetching students:', error);
+      }
+    };
+
+    fetchStudents();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Student List</h1>
+      <ul>
+        {students.map((student) => (
+          <li key={student.id}>
+            ID: {student.id}, Name: {student.firstname} {student.lastname}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
